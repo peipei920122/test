9a3346817a379168569ddad3ceab38f2385582c3
peipei920122 <169127260+peipei920122@users.noreply.github.com>
2024-06-04 13:51:14 +0800

    Add files via upload

--- try.py
+++ try.py
@@ -48,10 +48,10 @@ df_original = pd.read_pickle('2303.pkl')
 
 ##### 選擇資料區間
 st.subheader("選擇開始與結束的日期, 區間:2020/01/02 至 2024/06/03")
-start_date = st.text_input('選擇開始日期 (日期格式: 2020/01/02)', '2020/01/02')
-end_date = st.text_input('選擇結束日期 (日期格式: 2024/06/03)', '2024/06/03')
-start_date = datetime.datetime.strptime(start_date,'%Y/%m/%d')
-end_date = datetime.datetime.strptime(end_date,'%Y/%m/%d')
+start_date_str = st.text_input('選擇開始日期 (日期格式: 2020/01/02)', '2020/01/02')
+end_date_str = st.text_input('選擇結束日期 (日期格式: 2024/06/03)', '2024/06/03')
+start_date = datetime.datetime.strptime(start_date_str,'%Y/%m/%d')
+end_date = datetime.datetime.strptime(end_date_str,'%Y/%m/%d')
 # 使用条件筛选选择时间区间的数据
 df = df_original[(df_original['time'] >= start_date) & (df_original['time'] <= end_date)]
 
